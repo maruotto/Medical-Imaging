077e61112744135d9cff21671a12a064fb8abb29
maruotto <94022256+maruotto@users.noreply.github.com>
2022-12-23 19:49:24 +0100

    After intensity changes

--- MultiTask/train.py
+++ MultiTask/train.py
@@ -2,6 +2,7 @@
 # set the matplotlib backend so figures can be saved in the background
 import matplotlib
 import os
+
 matplotlib.use("Agg")
 
 # import the necessary packages
@@ -20,15 +21,17 @@ import argparse
 import torch
 import time
 import os
+import sys
 from training import config
 from training.model import UNet
 import torchmetrics.functional as f
 from training.GradNorm import GradNorm
+import math
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-l", "--letter", type=str, required=True, help="letter of the fold in capital case")
-ap.add_argument("-o", "--output", type=str, required=False, help="path to output trained model", default= "dice")
+ap.add_argument("-o", "--output", type=str, required=False, help="path to output trained model", default="dice")
 args = vars(ap.parse_args())
 
 # load the image and mask filepaths in a sorted manner
@@ -41,7 +44,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # create the train and test datasets
 trainData = MultiTaskDataset(csv_file=csv_file, root_dir=root_dir)
-print(f"[INFO {time.asctime( time.localtime(time.time()) )}] found {len(trainData)} examples in the training set...")
+print(f"[INFO {time.asctime(time.localtime(time.time()))}] found {len(trainData)} examples in the training set...")
 
 torch.manual_seed(12345)
 # create the training loader
@@ -50,35 +53,35 @@ trainLoader = DataLoader(trainData, shuffle=True, batch_size=config.BATCH_SIZE, 
 
 # calculate steps per epoch for training set
 trainSteps = len(trainData) // config.BATCH_SIZE
-    
+
 # initialize the train, validation, and test data loaders
 trainDataLoader = DataLoader(trainData, shuffle=True, batch_size=config.BATCH_SIZE)
 
 # initialize the model
-print(f"[INFO {time.asctime( time.localtime(time.time()) )}] initializing the model...")
-model = UNet(config.NUM_CHANNELS,config.NUM_CLASSES).to(device)
-    
+print(f"[INFO {time.asctime(time.localtime(time.time()))}] initializing the model...")
+model = UNet(config.NUM_CHANNELS, config.NUM_CLASSES).to(device)
+
 # initialize our optimizer and loss function
 opt = Adam(model.parameters(), lr=config.INIT_LR)
 lossFn = MultiTaskLoss()
 
 # initialize a dictionary to store training history
-metrics = {"mask_loss": 0.0,
-           "mask_acc": 0.0,
-           "label_loss":0.0,
-           "label_acc":0.0,
-           "intensity_loss":0.0,
-           "intensity_acc":0.0,
-            "mask_train_loss": [],
-           "label_train_loss": [],
-           "intensity_train_loss": [],
-           "mask_train_acc": [],
-           "label_train_acc": [],
-           "intensity_train_acc": []
-}
+metrics = {"mask_loss": np.array([]),
+           "mask_acc": np.array([]),
+           "label_loss": np.array([]),
+           "label_acc": np.array([]),
+           "intensity_loss": np.array([]),
+           "intensity_acc": np.array([]),
+           "mask_train_loss": np.array([]),
+           "label_train_loss": np.array([]),
+           "intensity_train_loss": np.array([]),
+           "mask_train_acc": np.array([]),
+           "label_train_acc": np.array([]),
+           "intensity_train_acc": np.array([]),
+           }
 
 # measure how long training is going to take
-print(f"[INFO {time.asctime( time.localtime(time.time()) )}] training the network...")
+print(f"[INFO {time.asctime(time.localtime(time.time()))}] training the network...")
 startTime = time.time()
 
 # set the model in training mode
@@ -89,19 +92,27 @@ pred = None
 # loop over our epochs
 for e in range(0, config.NUM_EPOCHS):
 #for e in range(0, 4):
-    i=0
+    print('config changed: old values as comments\nBCEwithLogits\nRemovedSigmoid and added before accuracy')
+    i = 0
+    metrics ["mask_loss"] =  np.array([]).astype(np.double)
+    metrics ["mask_acc"] =  np.array([])
+    metrics ["label_loss"] =  np.array([])
+    metrics["label_acc"] = np.array([])
+    metrics["intensity_loss"] = np.array([])
+    metrics["intensity_acc"] =  np.array([]).astype(np.double)
+
     # loop over the training set
     for x, y_mask, y_label, y_intensity in trainDataLoader:
         #print("iteration ", i, " of ", len(trainDataLoader))
-        i +=1
+        i += 1
+        sys.stdout.flush()
         # send the input to the device
         x = x.to(device)
         y_mask = y_mask.to(device)
-        y_label = [config.get_label_class(label) for label in y_label.tolist()]
         y_label = torch.Tensor(y_label).type(torch.uint8).to(device)
         y_intensity = torch.Tensor(y_intensity).to(device)
         # perform a forward pass and calculate the training loss
-        pred = model(x) # the model returns a list of three elements - three predictions:  mask, label, intensity
+        pred = model(x)  # the model returns a list of three elements - three predictions:  mask, label, intensity
         loss = lossFn(pred, y_mask, y_label, y_intensity)
         # zero out the gradients, perform the backpropagation step,
         # and update the weights
@@ -110,39 +121,38 @@ for e in range(0, config.NUM_EPOCHS):
         model.weights.data = model.weights.data * normalize_coeff
 
         # add the loss to the total training loss so far and
+        metrics["mask_loss"] = np.append(metrics["mask_loss"], float(loss[0].cpu()))
+        metrics["label_loss"] = np.append(metrics["label_loss"], float(loss[1].cpu()))
+        metrics["intensity_loss"] = np.append(metrics["intensity_loss"], float(loss[2].cpu()))
+
         # calculate the number of correct predictions
-        metrics["mask_loss"] = metrics["mask_loss"] + float(loss[0].cpu())
-        metrics["label_loss"] = metrics["label_loss"] + float(loss[1].cpu())
-        metrics["intensity_loss"] = metrics["intensity_loss"] + float(loss[2].cpu())
-
-        where = torch.Tensor(torch.where(pred[0] > torch.Tensor([config.THRESHOLD]).to(device), 1, 0)).type(torch.uint8)
-        metrics["mask_acc"] = metrics["mask_acc"] + float(f.dice(where.to(config.DEVICE), y_mask.type(torch.uint8)).cpu().item())
-        metrics["label_acc"] = metrics["label_acc"] + float((pred[1].argmax(1) == y_label).type(torch.float).sum().cpu().item())
-        metrics["intensity_acc"] = metrics["intensity_acc"] + float((pred[2].argmax(1) == y_intensity).type(torch.float).cpu().sum().item())
-
+        where = torch.sigmoid(pred[0])
+        where = torch.Tensor(torch.where(where> torch.Tensor([config.THRESHOLD]).to(device), 1, 0)).type(torch.uint8)
+        metrics["mask_acc"] = np.append(metrics["mask_acc"],float(f.dice(where.to(config.DEVICE), y_mask.type(torch.uint8)).cpu().item()))
+        metrics["label_acc"] = np.append(metrics["label_acc"], len(torch.where((pred[1].argmax(1) == y_label).cpu())[0]))
+        intensity_pred = torch.sigmoid(pred[2].squeeze())
+        where = torch.Tensor(torch.where(intensity_pred > torch.Tensor([config.THRESHOLD]).to(device), 1, 0)).type(torch.uint8)
+        metrics["intensity_acc"] = np.append(metrics["intensity_acc"], len(torch.where((where.squeeze() == y_intensity).cpu())[0]))
     # calculate the average training and validation loss
 
-    metrics["mask_loss"] /= len(trainDataLoader.dataset)
-    metrics["label_loss"] /= len(trainDataLoader.dataset)
-    metrics["intensity_loss"] /= len(trainDataLoader.dataset)
+    # update our training history
+    metrics["mask_train_acc"] = np.append(metrics["mask_train_acc"], metrics["mask_acc"].sum().item() / (config.BATCH_SIZE * len(trainDataLoader)))
+    metrics["label_train_acc"] = np.append(metrics["label_train_acc"], metrics["label_acc"].sum().item() / (config.BATCH_SIZE * len(trainDataLoader)))
+    metrics["intensity_train_acc"] = np.append(metrics["intensity_train_acc"], metrics["intensity_acc"].sum().item() / (config.BATCH_SIZE * len(trainDataLoader)))
 
-    # calculate the training and validation accuracy
-    metrics["mask_acc"] /= len(trainDataLoader.dataset)
-    metrics["label_acc"] /= len(trainDataLoader.dataset)
-    metrics["intensity_acc"] /= len(trainDataLoader.dataset)
+    metrics["mask_train_loss"] = np.append(metrics["mask_train_loss"], metrics["mask_loss"].sum().item() / (config.BATCH_SIZE * len(trainDataLoader)))
+    metrics["label_train_loss"] = np.append(metrics["label_train_loss"], metrics["label_loss"].sum().item() /  (config.BATCH_SIZE * len(trainDataLoader)))
+    metrics["intensity_train_loss"] = np.append(metrics["intensity_train_loss"],metrics["intensity_loss"].sum().item() / (config.BATCH_SIZE * len(trainDataLoader)))
 
-  	# update our training history
-    metrics["mask_train_acc"].append(metrics["mask_acc"])
-    metrics["label_train_acc"].append(metrics["label_acc"])
-    metrics["intensity_train_acc"].append(metrics["intensity_acc"])
-
-  	# print the model training and validation information
-    print(f"[INFO {time.asctime( time.localtime(time.time()) )}] ")
+    # print the model training and validation information
+    print(f"[INFO {time.asctime(time.localtime(time.time()))}] ")
     print("[INFO] EPOCH: {}/{}".format(e + 1, config.NUM_EPOCHS))
-    print("Mask loss: {:.6f}, Mask accuracy: {:.4f}".format(metrics["mask_loss"], metrics["mask_acc"]))
-    print("Label loss: {:.6f}, Label accuracy: {:.4f}".format(metrics["label_loss"], metrics["label_acc"]))
-    print("Intensity loss: {:.6f}, Intensity accuracy: {:.4f}".format(metrics["intensity_loss"], metrics["intensity_acc"]))
-        
+    print("Mask loss: {:.6f}, Mask accuracy: {:.4f}".format(metrics["mask_train_loss"][-1],
+                                                            metrics["mask_train_acc"][-1]))
+    print("Label loss: {:.6f}, Label accuracy: {:.4f}".format(metrics["label_train_loss"][-1],
+                                                              metrics["label_train_acc"][-1]))
+    print("Intensity loss: {:.6f}, Intensity accuracy: {:.4f}".format(metrics["intensity_train_loss"][-1],
+                                                                      metrics["intensity_train_acc"][-1]))
 # finish measuring how long training took
 endTime = time.time()
 
@@ -155,7 +165,7 @@ plt.title("Training Loss on Dataset")
 plt.xlabel("Epoch #")
 plt.ylabel("Loss")
 plt.legend(loc="lower left")
-plt.savefig(config.BASE_OUTPUT+ OUTPUT+"/lossPlot"+LETTER)
+plt.savefig(config.BASE_OUTPUT + OUTPUT + "/lossPlot" + LETTER)
 
 # plot the training loss and accuracy
 
@@ -167,10 +177,7 @@ plt.title("Training Accuracy on Dataset")
 plt.xlabel("Epoch #")
 plt.ylabel("Accuracy")
 plt.legend(loc="lower right")
-plt.savefig(config.BASE_OUTPUT+ OUTPUT+"/accuracyPlot"+LETTER)
-
-
-
+plt.savefig(config.BASE_OUTPUT + OUTPUT + "/accuracyPlot" + LETTER)
 
 # serialize the model to disk
-torch.save(model,config.BASE_OUTPUT + OUTPUT + "/model" +LETTER)
+torch.save(model, config.BASE_OUTPUT + OUTPUT + "/model" + LETTER)
